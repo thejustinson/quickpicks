8c6b51afaeb57ed86cf046b52b49b49ee54253f6
thejustinson <joelnwachukwu67@gmail.com>
2024-08-28 04:58:24 +0100

    inverted comma error

--- components/TopGamesGrid.tsx
+++ components/TopGamesGrid.tsx
@@ -4,7 +4,7 @@ const TopGamesGrid = () => {
   return (
     <div className="p-3 sm:p-5 space-y-2 sm:space-y-3">
       <div className="text-base sm:text-lg font-bold">
-        Today's top picks
+        Today&apos;s top picks
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 sm:gap-3">
         <TopGameItem />
